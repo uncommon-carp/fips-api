fec47ebb89b2f1b3fe48d1abccd135df5f7be7ca
Cory Sorel <csorel.creative@gmail.com>
2024-08-07 13:10:22 -0500

    fix express import

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,4 +1,5 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const County = require("../../models/county");
 
 router.get("/index", (__, res) => {
